09c693c4f97a65dd89239afed9e99536b3ab13d4
sena <mskucukk@outlook.com>
2021-03-10 23:58:32 +0300

    ASCII deger

---
+++ src/ASCIIDegerBulma.java
@@ -0,0 +1,10 @@
+public class AsciiDegerBulma {
+
+    public static void main(String[] String) {
+
+        char ch = 'a';
+        int ascii = ch;
+        System.out.println(" ASCII değeri: " + ascii);
+    }
+
+}
