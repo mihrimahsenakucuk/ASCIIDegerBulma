f4d2ec6ee85b4fea702bfc29bb22aa622ddf4f69
mihrimahsenakucuk <73707051+mihrimahsenakucuk@users.noreply.github.com>
2021-03-12 22:34:03 +0300

    Update ASCIIDegerBulma.java

--- src/ASCIIDegerBulma.java
+++ src/ASCIIDegerBulma.java
@@ -2,6 +2,7 @@ public class ASCIIDegerBulma {
 
     public static void main(String[] String) {
 
+        // a harfi ASCII değerini bulunuz.
         char ch = 'a';
         int ascii = ch;
         System.out.println(" ASCII değeri: " + ascii);
